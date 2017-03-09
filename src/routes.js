a97efcf4f3d14fc90e580f2f5113049406cd5952
Jordy Webb <jordywebb@Jordys-MacBook.local>
2017-03-09 06:46:16 -0800

    add IndexRoute

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import PostsIndex from './components/posts_index';
+
+const Greeting = () => {
+    return <div>Hey there</div>
+};
+
+export default (
+<Route path="/" component={App}>
+  <IndexRoute component={PostsIndex} />
+</Route>
+
+);
