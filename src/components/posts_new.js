f3331011a290453b41f23d09b601045063d5c04e
Jordy Webb <someone@someplace.com>
2017-03-10 14:36:34 -0800

    add fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    
     return(
-      <div>Create Form</div>
+      <form onSubmit={handleSubmit}>
+        <h3>Create a new post</h3>
+        <div className="form-group">
+          <label>Table</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+        Submit
+        </button>
+
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
