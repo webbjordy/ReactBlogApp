8c1b25067188fcc4b75a932631fd04cb2dd89767
Jordy Webb <jordy@thegrid.io>
2017-03-11 20:09:46 -0800

    Add cancel button

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 
 class PostsNew extends Component {
@@ -34,9 +35,8 @@ class PostsNew extends Component {
           </div>
         </div>
 
-        <button type="submit" className="btn btn-primary">
-        Submit
-        </button>
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
 
       </form>
     );
