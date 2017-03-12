ec2c67062badd7d9e24cafc3e61ec4a798c6e87d
Jordy Webb <jordy@thegrid.io>
2017-03-12 04:29:40 -0700

    add post index

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -7,6 +7,18 @@ class PostsIndex extends Component {
   componentWillMount() {
     this.props.fetchPosts();
   }
+
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
@@ -15,14 +27,21 @@ class PostsIndex extends Component {
           Add a post
           </Link>
         </div>
-        List of posts
+        <h3>Posts</h3>
+          <ul className="list-group">
+            { this.renderPosts() }
+          </ul>
       </div>
     );
   }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts.all }
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchPosts }, dispatch);
 }
 
-export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostsIndex);
